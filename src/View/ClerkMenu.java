6ced02eb6f1913e042b458c5f1e21a483b3e95cc
Calvin Schnierer <calvin.l.schnierer@gmail.com>
2018-04-22 14:26:17 +1000

    Working on ClerkMenu

--- src/View/ClerkMenu.java
+++ src/View/ClerkMenu.java
@@ -1,4 +1,16 @@
 package View;
 
 public class ClerkMenu {
+
+    public static void main(String[] args) {
+        DisplayMenu();
+    }
+
+    public static void DisplayMenu() {
+        System.out.println("1. See all sessions");
+        System.out.println("2. Find a movie");
+        System.out.println("3. Cinemas");
+        System.out.println("4. Add booking");
+        System.out.println("5. Delete booking");
+    }
 }
