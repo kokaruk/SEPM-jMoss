52dce0b49a1823adbd363214158d08f847f4480c
Calvin Schnierer <calvin.l.schnierer@gmail.com>
2018-04-23 19:17:55 +1000

    BookingClerkMenu

--- src/view/BookingClerkMainMenu.java
+++ src/view/BookingClerkMainMenu.java
@@ -1,4 +1,7 @@
 package view;
+/**
+ * @author Calvin and Dimi
+ */
 
 import java.io.File;
 import java.io.IOException;
@@ -6,6 +9,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class BookingClerkMainMenu extends JMossView {
+
+    public String MenuOptions = "1. See All Movies\n" +
+                                "2. Find A Movie\n" +
+                                "3. Cinemas\n" +
+                                "4.  Add Booking\n" +
+                                "5. Delete Booking\n";
 
     public BookingClerkMainMenu() {
         String pathToAsciFile = "assets/ascii_art.txt";
@@ -15,7 +24,9 @@ public class BookingClerkMainMenu extends JMossView {
 
         try {
             String asciArt = new String(Files.readAllBytes(Paths.get(pathToAsciFile)));
-            setMyContent(asciArt);
+            String MenuText = asciArt + "\n" + MenuOptions;
+            setMyContent(MenuText);
+
         } catch (IOException ex) {
             System.out.println("Asset header not found");
         }
