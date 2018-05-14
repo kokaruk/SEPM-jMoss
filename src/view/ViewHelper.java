bfe4710477eee6f3f50e6c80f21e23391de796c5
rmit-s3668804-Dzmitry-Kakaruk <s3668804@student.rmit.edu.au>
2018-05-14 19:07:48 +1000

    win fix

--- src/view/ViewHelper.java
+++ src/view/ViewHelper.java
@@ -7,15 +7,43 @@ package view;
  */
 public class ViewHelper {
 
-    static final String ANSI_RESET = "\u001B[0m";
-    static final String ANSI_BLACK = "\u001B[30m";
-    static final String ANSI_RED = "\u001B[31m";
-    static final String ANSI_GREEN = "\u001B[32m";
-    static final String ANSI_YELLOW = "\u001B[33m";
-    static final String ANSI_BLUE = "\u001B[34m";
-    static final String ANSI_PURPLE = "\u001B[35m";
-    static final String ANSI_CYAN = "\u001B[36m";
-    static final String ANSI_WHITE = "\u001B[37m";
+    static final String ANSI_RESET;
+    static final String ANSI_BLACK;
+    static final String ANSI_RED;
+    static final String ANSI_GREEN;
+    static final String ANSI_YELLOW;
+    static final String ANSI_BLUE;
+    static final String ANSI_PURPLE;
+    static final String ANSI_CYAN;
+    static final String ANSI_WHITE;
+
+    static{
+        String osName = System.getProperty("os.name").toLowerCase();
+        boolean isMacOs = osName.startsWith("mac os x");
+        if(isMacOs){
+            ANSI_RESET = "\u001B[0m";
+            ANSI_BLACK = "\u001B[30m";
+            ANSI_RED = "\u001B[31m";
+            ANSI_GREEN = "\u001B[32m";
+            ANSI_YELLOW = "\u001B[33m";
+            ANSI_BLUE = "\u001B[34m";
+            ANSI_PURPLE = "\u001B[35m";
+            ANSI_CYAN = "\u001B[36m";
+            ANSI_WHITE = "\u001B[37m";
+        } else {
+            ANSI_RESET = "";
+            ANSI_BLACK = "";
+            ANSI_RED = "";
+            ANSI_GREEN = "";
+            ANSI_YELLOW = "";
+            ANSI_BLUE = "";
+            ANSI_PURPLE = "";
+            ANSI_CYAN = "";
+            ANSI_WHITE = "";
+        }
+
+    }
+
 
     // no instance please
     private ViewHelper(){}
